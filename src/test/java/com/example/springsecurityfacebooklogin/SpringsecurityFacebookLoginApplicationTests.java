8508d4ba7edc7bbdb282f548153e48124df16401
Luk Pheakdey <luk.pheakdey@gmail.com>
2020-06-29 11:55:24 -0400

    Spring Security using OAuth with facebook login

---
+++ src/test/java/com/example/springsecurityfacebooklogin/SpringsecurityFacebookLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityfacebooklogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityFacebookLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
